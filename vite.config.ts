9bbf3571a809e19e51c2f2bb32b1bb47611575a7
yohawing <yohawing@gmail.com>
2026-05-07 14:36:25 +0900

    build: move app html entry under scripts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: "scripts",
   plugins: [react()],
   clearScreen: false,
+  publicDir: "../public",
   build: {
+    outDir: "../dist",
+    emptyOutDir: true,
     rollupOptions: {
       output: {
         manualChunks(id) {
